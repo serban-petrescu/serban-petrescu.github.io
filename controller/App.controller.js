6af8e9bfb3a9f4a60aa7acabc2c97ae58349baac
Serban Petrescu <serban.petrescu@msg-systems.com>
2016-07-15 07:10:32 +0000

    Major site overhaul.

---
+++ controller/App.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+	
+	return Controller.extend("portfolio.controller.App", {
+		
+		onInit: function() {
+			this.getView().addStyleClass(this.getOwnerComponent().getContentDensityClass());
+		}
+		
+	});
+});
